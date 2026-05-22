af72f6ca25a45ec0a6e8170c997ded5002c0e09e
triton215 <vinsadi215@gmail.com>
2026-05-22 18:18:51 +0700

    first commit

---
+++ TugasPer13.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Tugas Cloud Computing" << endl;
+    cout << "Hello World" << endl;
+    cout << "Adi Ryan Gunawana. S" << endl;
+    cout << "241011400907" << endl;
+    cout << "04TPLM003" << endl;
+    return 0;
+}
